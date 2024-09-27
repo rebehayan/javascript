135225758767343bc3c93fc658e27bd6a1a5eecb
rebehayan <rebehayan@gmail.com>
2024-09-27 23:43:27 +0900

    basic

---
+++ 00Basic/06-event.js
@@ -0,0 +1,56 @@
+const message = document.querySelector(".message");
+const click = document.querySelector(".click");
+const enter = document.querySelector(".enter");
+const leave = document.querySelector(".leave");
+const inputText = document.querySelector(".input input");
+const change = document.querySelector(".change select");
+const submit = document.querySelector(".submit");
+const submitID = document.querySelector(".submit .id");
+const submitNickname = document.querySelector(".submit .nickname");
+const target = document.querySelector(".target .parent");
+
+const log = function (text) {
+  const div = document.createElement("div");
+  div.innerHTML = text;
+
+  message.appendChild(div);
+  message.scrollTop = message.scrollHeight;
+};
+
+click.addEventListener("click", function () {
+  log("click");
+});
+
+enter.addEventListener("mouseenter", function () {
+  log("mouseenter");
+});
+
+leave.addEventListener("mouseleave", function () {
+  log("mouseleave");
+});
+
+inputText.addEventListener("input", function (e) {
+  log(e.target.value);
+});
+
+change.addEventListener("change", function (e) {
+  log(e.target.value);
+});
+
+submit.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const id = submitID.value;
+  const nickname = submitNickname.value;
+
+  log("아이디는" + id + "/ 닉네임은" + nickname);
+});
+
+target.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const isTarget = e.target.id;
+  const isCurrentTarget = e.currentTarget.id;
+
+  log(isTarget);
+  // log(isCurrentTarget);
+});
