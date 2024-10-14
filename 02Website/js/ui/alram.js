9662e254cbee7518cd714c07eaa6ee788f0dc180
rebehayan <rebehayan@gmail.com>
2024-10-14 12:03:19 +0900

    website

--- 02Website/js/ui/alram.js
+++
@@ -1,8 +0,0 @@
-export const alram = () => {
-  const alramList = document.querySelectorAll("#alram li");
-  const alramText = document.querySelector(".profile__alram");
-  const alramCount = alramList.length;
-
-  console.log(alramCount);
-  alramText.innerText = alramCount;
-};
