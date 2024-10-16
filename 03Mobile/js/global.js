a2a462b906ae60a2aa7387ee3f9745fc06cf788a
rebehayan <rebehayan@gmail.com>
2024-10-16 20:07:15 +0900

    mobile

--- 03Mobile/js/global.js
+++ 03Mobile/js/global.js
@@ -1,7 +1,7 @@
 import { Menu } from "./ui/Menu.js";
 import { Tab } from "./ui/Tab.js";
 import { FixBottom } from "./ui/FixBottom.js";
-import { CopyText } from "./ul/CopyText.js";
+import { CopyText } from "./ui/CopyText.js";
 import { Addfile } from "./ui/Addfile.js";
 
 Menu();
