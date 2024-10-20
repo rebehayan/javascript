9b3caf19af297cfb255bd517427c52cc570b6b26
rebehayan <rebehayan@gmail.com>
2024-10-20 13:46:22 +0900

    basic

--- 00Basic/08-Object.js
+++ 00Basic/08-Object.js
@@ -1,27 +1,3 @@
 // 객체생성
 // 객체접근
 // 선택적 체이닝
-const tabData = {
-  title: "제목입니다.",
-  view: 1200,
-  like: 5,
-  lectures: 30,
-  price: 20000,
-};
-
-const { title, view, like, lectures, price } = tabData;
-
-const tabContent = document.querySelector(".tab-content");
-
-tabContent.innerHTML = `
-      <div>
-        <img src="./images/001.jpg" alt="">
-        <strong class="title">${title}</strong>
-        <div class="info">
-          <span class="view">${view.toLocaleString()}</span>
-          <span class="like">${like.toLocaleString()}</span>
-        </div>
-        <div class="lectures">${lectures.toLocaleString()} Lectures</div>
-        <div class="price">${price.toLocaleString()}원</div>
-      </div>
-  `;
---
+++ 00Basic/10-event.js
@@ -0,0 +1,7 @@
+// 이벤트제거
+// 캡처링과 버블링
+// 이벤트 종류
+// target, currentTarget
+// 키보드 keyCode
+// 기본동작 방지
+// animationend
